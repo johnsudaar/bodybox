038f7218788cca1dc980c52087568d2c43d93cb5
Powkachu <pauline.kobersi@gmail.com>
2015-11-21 12:34:40 +0100

    Maj commande avec trame

--- app/helpers/commande_helper.rb
+++ app/helpers/commande_helper.rb
@@ -1,2 +1,11 @@
 module CommandeHelper
+  def get_menu_class(target,current)
+    if target > current
+      return "disabled"
+    elsif target == current
+      return "active"
+    else
+      return "complete"
+    end
+  end
 end
