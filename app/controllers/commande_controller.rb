8632a4db0213c8140de03cc95232716fd0dfd0d2
Powkachu <pauline.kobersi@gmail.com>
2015-11-21 16:11:11 +0100

    Affichage plat, selection ingrédient

--- app/controllers/commande_controller.rb
+++ app/controllers/commande_controller.rb
@@ -5,4 +5,18 @@ class CommandeController < ApplicationController
   def choisir
     @ingredients = Ingredient.all.group_by(&:categorie)
   end
+
+  def menu 
+    ingredients = []
+    
+    params['ingredients'].each do | ingredient, _ |
+      ingredients << ingredient.to_i
+    end
+
+    puts(ingredients)
+    @plats = Plat.get_all_by_ingredients(ingredients)
+
+    puts("=========================================================================")
+    puts(@plats)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   get 'commande/' => 'commande#index'
   get 'commande/choisir' => 'commande#choisir'
+  post 'commande/menu' => 'commande#menu'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
