3b68d9fcc6a57c3c298e2f259d5082f74868c5ec
johnsudaar <jonathan.hurter@laposte.net>
2015-11-21 18:24:30 +0100

    Storing into session

--- app/controllers/commande_controller.rb
+++ app/controllers/commande_controller.rb
@@ -3,12 +3,16 @@ class CommandeController < ApplicationController
   end
 
   def choisir
+    @session[:proteines_value] = params["commande"]["proteines"]
+    @session[:glucides_value] = params["commande"]["glucides"]
+    @session[:legumes_value] = params["commande"]["legumes"]
+
     @ingredients = Ingredient.all.group_by(&:categorie)
   end
 
-  def menu 
+  def menu
     ingredients = []
-    
+
     params['ingredients'].each do | ingredient, _ |
       ingredients << ingredient.to_i
     end
